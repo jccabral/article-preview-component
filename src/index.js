4e6746d8a936eaccc8de4c48c8c06ad57ed85bd1
Juan Cabral <cabraljcruz@gmail.com>
2020-07-23 18:40:35 -0300

    changed favicon & modified public folder files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// SCSS Styles
 import './scss/index.scss';
 import './scss/normalizer.scss';
+
+// Components
 import App from './App';
 
 ReactDOM.render(
-    <App />,
+  <App />,
   document.getElementById('root')
 );
 
