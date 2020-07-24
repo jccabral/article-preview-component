1f6a234cc5de9f2558221307abcf72f66e7637f6
Juan Cabral <cabraljcruz@gmail.com>
2020-07-24 01:24:39 -0300

    Added article image

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+console.log(process.env.PUBLIC_URL)
+
 const Card = () => {
     return(
         <section className="card">
             <div className="card__image">
-                <img className="card_image--content"/>
+                <img src={process.env.PUBLIC_URL + '/static/img/drawers.jpg'} alt="drawer" className="card__image--content"/>
             </div>
 
         </section>
