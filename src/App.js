aff549bf9ac599ef2dcdd874a2f8e8260285732b
Juan Cabral <cabraljcruz@gmail.com>
2020-07-23 22:35:49 -0300

    created card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Card from './components/Card';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <main className="App">
+      <Card />
+    </main>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+    return(
+        <section className="card">
+            <div className="card__image">
+                <img className="card_image--content"/>
+            </div>
+
+        </section>
+    )
+};
+
+export default Card;
