c82022378e2785c5d3a66db0d8ad01355a3c42e5
Juan Cabral <cabraljcruz@gmail.com>
2020-07-24 22:29:21 -0300

    Added share icons and style to share menu component

--- src/components/ShareMenu.js
+++ src/components/ShareMenu.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const ShareMenu = () => {
+
+    const socialIcons = [
+      'icon-facebook.svg',  
+      'icon-twitter.svg', 
+      'icon-pinterest.svg' 
+    ]
+
     return (
         <div className="share-menu">
-            <p>SHARE</p>
+            <p className="share-menu__text">SHARE</p>
+            {
+                socialIcons.map(icon => (
+                    <img src={process.env.PUBLIC_URL + '/static/svg/' + icon} className="share-menu__icon"></img>
+                ))
+            }
         </div>
     )
 };
